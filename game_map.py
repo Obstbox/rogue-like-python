afd2934614295eb77a326443ec91a8e66c9acfa8
ivan pashchenko <obst.box@gmail.com>
2023-11-19 13:22:12 +0500

    Pt.4 FOV + unexplored map done

--- game_map.py
+++ game_map.py
@@ -3,17 +3,30 @@ from tcod.console import Console
 
 import tile_types
 
+
 class GameMap:
     def __init__(self, width: int, height: int):
         self.width, self.height = width, height
         self.tiles = np.full((width, height), fill_value=tile_types.wall, order="F")
 
-    def in_bounds(self, x:int, y:int) -> bool:
+        # visible now
+        self.visible = np.full((width, height), fill_value=False, order="F")
+        # seen before
+        self.explored = np.full((width, height), fill_value=False, order="F")
+
+    def in_bounds(self, x: int, y: int) -> bool:
         """ return true if x and y are inside of the bounds of this map."""
         return 0 <= x < self.width and 0 <= y < self.height
 
     def render(self, console: Console) -> None:
-        console.rgb[0:self.width, 0:self.height] = self.tiles["dark"]
-
-
-
+        """
+        Renders the map.
+        light for visible tiles
+        dark for explored but not visible
+        SHROUD for rest
+        """
+        console.rgb[0:self.width, 0:self.height] = np.select(
+            condlist=[self.visible, self.explored],
+            choicelist=[self.tiles["light"], self.tiles["dark"]],
+            default=tile_types.SHROUD
+        )
