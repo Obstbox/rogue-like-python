955b806c9fdca696a12824327bfcd1fe20d1aa80
ivan pashchenko <obst.box@gmail.com>
2023-11-24 00:54:46 +0500

    BumpAction class нужен только для того, чтобы решить, какой класс вернуть: MeleeAction или MovementAction решение принимается на основании того, есть ли у нас на пути Entity, блокирующая движение

--- rogue/actions.py
+++ rogue/actions.py
@@ -34,6 +34,17 @@ class ActionWithDirection(Action):
         raise NotImplementedError()
 
 
+class MeleeAction(ActionWithDirection):
+    def perform(self, engine: Engine, entity: Entity) -> None:
+        dest_x = entity.x + self.dx
+        dest_y = entity.y + self.dy
+        target = engine.game_map.get_blocking_entity_at_location(dest_x, dest_y)
+        if not target:
+            return    # No entity to attack
+
+        print(f"You kick the {target.name}, much to its annoyance!")
+
+
 class MovementAction(ActionWithDirection):
     def perform(self, engine: Engine, entity: Entity) -> None:
         dest_x = entity.x + self.dx
@@ -49,3 +60,15 @@ class MovementAction(ActionWithDirection):
             return    # destination blocked by an entity
 
         entity.move(self.dx, self.dy)
+
+
+class BumpAction(ActionWithDirection):
+    def perform(self, engine: Engine, entity: Entity) -> None:
+        dest_x = entity.x + self.dx
+        dest_y = entity.y + self.dy
+
+        if engine.game_map.get_blocking_entity_at_location(dest_x, dest_y):
+            return MeleeAction(self.dx, self.dy).perform(engine, entity)
+
+        else:
+            return MovementAction(self.dx, self.dy).perform(engine, entity)
