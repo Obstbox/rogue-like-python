7cf9ee09a1b5e0ff395288ebfaeb1a9047b54623
ivan pashchenko <obst.box@gmail.com>
2023-11-27 22:00:18 +0500

    now player and enemies can attack and kill each other but there are two issues

--- rogue/components/fighter.py
+++ rogue/components/fighter.py
@@ -1,7 +1,16 @@
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
 from rogue.components.base_component import BaseComponent
+
+if TYPE_CHECKING:
+    from rogue.entity import Actor
 
 
 class Fighter(BaseComponent):
+    entity: Actor
+
     def __init__(self, hp: int, defense: int, power: int):
         self.max_hp = hp
         self._hp = hp
@@ -16,3 +25,20 @@ class Fighter(BaseComponent):
     def hp(self, value: int) -> None:
         # ensures hp always between 0 and max_hp
         self._hp = max(0, min(value, self.max_hp))
+        if self._hp == 0 and self.entity.ai:
+            self.die()
+
+    def die(self) -> None:
+        if self.engine.player is self.entity:
+            death_message = "You died!"
+        else:
+            death_message = f"{self.entity.name} is dead!"
+
+        self.entity.char = "%"
+        # self.entity.color = (191, 0, 0)
+        self.entity.color = (120, 120, 120)
+        self.entity.blocks_movement = False
+        self.entity.ai = None
+        self.entity.name = f"remains of {self.entity.name}"
+
+        print(death_message)
