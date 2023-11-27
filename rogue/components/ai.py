c1cea121bab64dfba8d8bfbb0075361334a9ffee
ivan pashchenko <obst.box@gmail.com>
2023-11-27 14:31:12 +0500

    ai upd+

--- rogue/components/ai.py
+++ rogue/components/ai.py
@@ -17,7 +17,7 @@ class BaseAI(Action, BaseComponent):
     # be using AI to act will have to have an AI class that inherits from this one.
 
     entity: Actor
-    
+
     def perfrom(self) -> None:
         raise NotImplementedError()
 
@@ -54,4 +54,26 @@ class BaseAI(Action, BaseComponent):
 
 
 class HostileEnemy(BaseAI):
+    def __init__(self, entity: Actor):
+        super().__init__(entity)
+        self.path: List[Tuple[int, int]] = []
 
+    def perform(self) -> None:
+        target = self.engine.player
+        dx = target.x - self.entity.x
+        dy = target.y - self.entity.y
+        distance = max(abs(dx), abs(dy))    # Chebyshev distance.
+
+        if self.engine.game_map.visible[self.entity.x, self.entity.y]:
+            if distance <= 1:
+                return MeleeAction(self.entity, dx, dy).perform()
+
+            self.path = self.get_path_to(target.x, target.y)
+
+        if self.path:
+            dest_x, dest_y = self.path.pop(0)
+            return MovementAction(
+                self.entity, dest_x - self.entity.x, dest_y - self.entity.y,
+            ).perform()
+
+        return WaitAction(self.entity).perform()
